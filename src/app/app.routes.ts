cff2bcbe6bbedb094686b19d3b3490d6c54d0d94
Achu-Exp <achu73743@gmail.com>
2024-11-27 10:54:11 +0530

    Project Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Layout/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',component:DashboardComponent
+  }
+];
