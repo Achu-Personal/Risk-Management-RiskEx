b827002d7040f4aa12705528c19e26166274ab56
Shamna Sherin <shamna.sherin@experionglobal.local>
2024-12-03 09:57:15 +0530

    side bar created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './Layout/dashboard/dashboard.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
 
 export const routes: Routes = [
   {
     path:'',component:DashboardComponent
-  }
+  },
+  {path:'sidebar',component:SidebarComponent}
 ];
