259dfae656a9bb72939676ffdf4029561206af3a
Shamna Sherin <shamna.sherin@experionglobal.local>
2024-12-05 22:02:59 +0530

    set route path

---
+++ src/app/Pages/wild-component/wild-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wild-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './wild-component.component.html',
+  styleUrl: './wild-component.component.scss'
+})
+export class WildComponentComponent {
+
+}
