7d5598c2cc7fdf5a8949f2856dfb9a466a23f85e
Hisham E <hisham.e@experionglobal.local>
2024-12-06 10:06:44 +0530

    feat:parftial add form for risk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,6 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './Layout/dashboard/dashboard.component';
-import { SidebarComponent } from './Components/sidebar/sidebar.component';
-import { RegisterRiskComponent } from './Pages/register-risk/register-risk.component';
 
 export const routes: Routes = [
-  {
-    path:'',component:DashboardComponent
-  },
-  {path:'sidebar',component:SidebarComponent},
+
 
 ];
