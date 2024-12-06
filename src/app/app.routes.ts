4e8fca5b8da877777f69bb9b14155a6d014e170c
Vivek E N <vivek.en@experionglobal.local>
2024-12-06 09:29:45 +0530

    developer pull

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './Layout/dashboard/dashboard.component';
+import { ForgetpswrdComponent } from './Pages/forgetpswrd/forgetpswrd.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 export const routes: Routes = [
   {
     path:'',component:DashboardComponent
+  },
+  {
+    path:"forgetpassword",component:ForgetpswrdComponent
+  },
+  {
+    path:"Login",component:LoginComponent
   }
 ];
