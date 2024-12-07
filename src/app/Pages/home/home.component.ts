cd6e726bce2e9a178bd7641ddec21722d8c9d957
Achu-Exp <achu73743@gmail.com>
2024-12-07 16:24:48 +0530

    ...

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { BodyContainerComponent } from "../../Components/body-container/body-con
 import { SearchboxComponent } from "../../UI/searchbox/searchbox.component";
 import { ButtonComponent } from "../../UI/button/button.component";
 import { DropdownComponent } from "../../UI/dropdown/dropdown.component";
+import { DepartmentDropdownComponent } from "../../Components/department-dropdown/department-dropdown.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BodyContainerComponent, ButtonComponent],
+  imports: [BodyContainerComponent, ButtonComponent, DepartmentDropdownComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
