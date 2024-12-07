3da2d4d481e424c4c6733d7624ffec0321261201
Macluster <deepakjudejohn@gmail.com>
2024-12-07 12:10:02 +0530

    risk view page fix

--- src/app/Pages/view-risk/view-risk.component.ts
+++ src/app/Pages/view-risk/view-risk.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { RiskBasicDetailsCardComponent } from "../../Components/risk-basic-details-card/risk-basic-details-card.component";
 import { BodyContainerComponent } from "../../Components/body-container/body-container.component";
 import { RiskDetailsSection2Component } from "../../Components/risk-details-section2/risk-details-section2.component";
+import { RiskDetailsSection3MitigationComponent } from "../../Components/risk-details-section3-mitigation/risk-details-section3-mitigation.component";
 
 @Component({
   selector: 'app-view-risk',
   standalone: true,
-  imports: [RiskBasicDetailsCardComponent, BodyContainerComponent, RiskDetailsSection2Component],
+  imports: [RiskBasicDetailsCardComponent, BodyContainerComponent, RiskDetailsSection2Component, RiskDetailsSection3MitigationComponent],
   templateUrl: './view-risk.component.html',
   styleUrl: './view-risk.component.scss'
 })
---
+++ src/app/UI/likelihood-impact-card/likelihood-impact-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-likelihood-impact-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './likelihood-impact-card.component.html',
+  styleUrl: './likelihood-impact-card.component.scss'
+})
+export class LikelihoodImpactCardComponent {
+
+}
