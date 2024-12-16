fb1840bcb3e25cf59c1b472e1e9f609a2674f2fa
Shamna Sherin <shamna.sherin@experionglobal.local>
2024-12-16 16:50:43 +0530

    Approval table layout changed

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -10,10 +10,10 @@ import { NavigationEnd, Router, RouterLink } from '@angular/router';
 })
 export class SidebarComponent {
   menuItems = [
-    { id: 1, label: 'Home', icon: 'bi-house', route: '/home', active: false },
+    { id: 1, label: 'Dashboard', icon: 'bi-house', route: '/home', active: false },
     { id: 2, label: 'Users', icon: 'bi-people', route: '/users', active: false },
     { id: 3, label: 'History', icon: 'bi-clock-history', route: '/history', active: false },
-    { id: 4, label: 'Reports', icon: 'bi-bar-chart', route: '/reports', active: false },
+    // { id: 4, label: 'Reports', icon: 'bi-bar-chart', route: '/reports', active: false },
     { id: 5, label: 'Approvals', icon: 'bi-check2-square', route: '/approvaltable', active: false },
   ];
 
