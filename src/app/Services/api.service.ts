896ac9081ba691fc21dfebe30465547e72dfcd9f
Achu-Exp <achu73743@gmail.com>
2024-12-17 09:18:29 +0530

    Bug fixed

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -12,33 +12,53 @@ export class ApiService {
 
 
 constructor(private http:HttpClient) { }
-  //Just for now to test can be removed later
-  getAllRisk()
-  {
-      return this.http.get("/data/getAllRisk.json");
+   //Just for now to test can be removed later
+   getAllRisk()
+   {
+       return this.http.get("/data/getAllRisk.json");
 
-  }
-  getRiskType(){
-    return this.http.get(`data/type-dropdown.json`)
-  }
+   }
+   getRiskType(){
+     return this.http.get(`data/type-dropdown.json`)
+   }
 
-  getRiskCurrentAssessment(){
-    return this.http.get(`data/assessment-dropdown.json`)
-  }
+   getRiskCurrentAssessment(){
+     return this.http.get(`data/assessment-dropdown.json`)
+   }
 
-  getRiskById(id:number)
-  {
-    return  this.http.get("/data/getAllRisk.json").pipe(map((data:any)=>{
-      console.log(data)
-     return  data.filter((item:any)=>item.id===id)[0]
-    }));
-  }
+   getRiskById(id:number)
+   {
+     return  this.http.get("/data/getAllRisk.json").pipe(map((data:any)=>{
+       console.log(data)
+      return  data.filter((item:any)=>item.id===id)[0]
+     }));
+   }
+   getDepartment() {
+     return this.http.get<department[]>('https://localhost:7150/api/Department/GetAllDepartments');
+   }
 
-  getRisk()
-  {
-    console.log("hai")
-      return this.http.get(`data/getRisk.json`);
+   addNewDepartment(department: any): Observable<{ message: string }> {
+     return this.http.post<{ message: string }>('https://localhost:7150/api/Department/AddDepartment', department);
+   }
 
+   getProjects(departmentName: string) {
+     return this.http.get<project[]>(`https://localhost:7150/api/Project/GetProjectsByDepartment/${departmentName}`);
+   }
 
-  }
-}
+   gettabledata(){
+     return this.http.get(`data/tabledata.json`)
+   }
+   getFilteredData(department: any) {
+     return this.http.get(`data/tabledata.json`).pipe(
+       map((data:any) => {
+         const filteredData = data.filter((item:any) => item.department.toLowerCase() === String( department).toLowerCase());
+         return filteredData;
+       })
+     );
+   }
+   addNewProject(project:any){
+     return this.http.post("https://localhost:7150/api/Project/AddProject",project)
+   }
+
+ }
+ 
