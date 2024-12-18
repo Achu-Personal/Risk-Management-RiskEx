f113b9f506d2657e26de79553da8ef51785f55c3
Shamna Sherin <shamna.sherin@experionglobal.local>
2024-12-18 10:41:15 +0530

    reusable table design changed

--- src/app/Pages/approval-table/approval-table.component.ts
+++ src/app/Pages/approval-table/approval-table.component.ts
@@ -17,10 +17,64 @@ export class ApprovalTableComponent {
 
   
 headerData:any=[
-  "SI NO"," RiskID","Risk","Description","EndDate","Type","Current Risk Rating","Status",
+  "SI NO"," Risk Id","Risk","Description","End Date","Type","CRR","Status",
 ];
 
 tableBody:approvalTableBody[]=[
+  {
+    SINo: 1,
+    RiskId: 'R001',
+    Risk: 'Inside Threat',
+    Description: 'Potential unauthorized access to sensitive data.',
+    EndDate: new Date('2024-12-31'),
+    Type: 'Cybersecurity',
+    CurrentRating: 8,
+    Status: 'Pending'
+    
+  },
+  {
+    SINo: 2,
+    RiskId: 'R002',
+    Risk: 'Third Party Vendor Breach',
+    Description: 'Critical application unavailability due to server failure.',
+    EndDate: new Date('2024-11-30'),
+    Type: 'Operational',
+    CurrentRating: 7,
+    Status: 'Pending'
+    
+  },
+  {
+    SINo: 3,
+    RiskId: 'R003',
+    Risk: 'Regulatory Compliance',
+    Description: 'Non-compliance with new tax regulations.',
+    EndDate: new Date('2024-10-15'),
+    Type: 'Legal',
+    CurrentRating: 5,
+    Status: 'Pending'
+    
+  },
+  {
+    SINo: 4,
+    RiskId: 'R004',
+    Risk: 'Vendor Dependency',
+    Description: 'Over-reliance on a single vendor for critical operations.',
+    EndDate: new Date('2024-09-01'),
+    Type: 'Strategic',
+    CurrentRating: 6,
+    Status: 'Pending'
+    
+  },
+  {
+    SINo: 5,
+    RiskId: 'R005',
+    Risk: 'Market Fluctuations',
+    Description: 'Adverse impact on revenue due to market changes.',
+    EndDate: new Date('2024-12-15'),
+    Type: 'Financial',
+    CurrentRating: 4,
+    Status: 'Pending'
+  },
   {
     SINo: 1,
     RiskId: 'R001',
