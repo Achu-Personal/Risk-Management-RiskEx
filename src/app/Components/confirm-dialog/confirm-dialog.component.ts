3f4751e33ba0d1af622915129156d0c0e744a756
Shamna Sherin <shamna.sherin@experionglobal.local>
2024-12-26 13:04:43 +0530

    Button style changes

--- src/app/Components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/Components/confirm-dialog/confirm-dialog.component.ts
@@ -1,18 +1,26 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { StyleButtonComponent } from "../../UI/style-button/style-button.component";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
-  imports: [MatButtonModule, CommonModule, MatDialogModule, StyleButtonComponent],
+  imports: [MatButtonModule, CommonModule, MatDialogModule, StyleButtonComponent, FormsModule, ReactiveFormsModule,],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.scss'
 })
 export class ConfirmDialogComponent {
+  
+  @Input() IsCommentRequiered:boolean=false;
+
+   isButtonClicked:boolean = false;
+    commentForm = new FormGroup({
+    approve: new FormControl('')});
+    
   constructor(
     public dialogRef: MatDialogRef<ConfirmDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { message: string }
@@ -23,6 +31,10 @@ export class ConfirmDialogComponent {
   
   onConfirm(): void {
     this.dialogRef.close(true);
+ 
+
+      console.log(this.commentForm.value);
+    
   }
 
   onCancel(): void {
