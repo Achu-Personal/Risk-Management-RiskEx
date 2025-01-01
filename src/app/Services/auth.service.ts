5773f96908e4968442953c0c7eaa5f6cc37103da
Macluster <deepakjudejohn@gmail.com>
2025-01-01 20:32:25 +0530

    assignment page api integrated

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -130,4 +130,12 @@ export class AuthService {
     //   this.router.navigate(['/home']);
     // }
   }
+
+  getToken()
+  {
+    const token = localStorage.getItem('token');
+    console.log(token);
+
+    return token;
+  }
 }
