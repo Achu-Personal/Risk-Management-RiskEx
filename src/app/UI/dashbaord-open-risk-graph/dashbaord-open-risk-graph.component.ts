5e71e64ef2e4578e8608c11dca42d2b71a3da636
Bismi S <bismi.s@experionglobal.local>
2025-01-09 12:31:56 +0530

    dashbaord bug fix

--- src/app/UI/dashbaord-open-risk-graph/dashbaord-open-risk-graph.component.ts
+++ src/app/UI/dashbaord-open-risk-graph/dashbaord-open-risk-graph.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashbaord-open-risk-graph',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashbaord-open-risk-graph.component.html',
   styleUrl: './dashbaord-open-risk-graph.component.scss'
 })
