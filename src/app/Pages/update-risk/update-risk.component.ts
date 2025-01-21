62735152aec9c7320fb0017e0d32687ab66daf79
Achu-Exp <achu73743@gmail.com>
2025-01-21 10:12:27 +0530

    chnages

--- src/app/Pages/update-risk/update-risk.component.ts
+++ src/app/Pages/update-risk/update-risk.component.ts
@@ -52,9 +52,9 @@ ngOnInit(){
     this.overallRiskRatingBefore = params['overallRiskRatingBefore'];
 
   });
-  if( this.riskType='Quality'){
+  if( this.riskType==='Quality'){
     this.riskTypeId=1
-  }else if(this.riskType='Security'){
+  }else if(this.riskType==='Security'){
     this.riskTypeId=2
   }else{
     this.riskTypeId=3
