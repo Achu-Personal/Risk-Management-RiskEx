9ba27d2942ade4e5c238df78dff9d0e925a746e5
Achu-Exp <achu73743@gmail.com>
2025-01-24 16:27:28 +0530

    wild card component added

--- src/app/Pages/wild-component/wild-component.component.ts
+++ src/app/Pages/wild-component/wild-component.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { StyleButtonComponent } from "../../UI/style-button/style-button.component";
 
 @Component({
   selector: 'app-wild-component',
   standalone: true,
-  imports: [],
+  imports: [StyleButtonComponent],
   templateUrl: './wild-component.component.html',
   styleUrl: './wild-component.component.scss'
 })
 export class WildComponentComponent {
-
+  goBack(): void {
+    window.history.back();
+  }
 }
