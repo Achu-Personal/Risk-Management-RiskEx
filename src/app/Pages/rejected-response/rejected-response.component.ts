1920428d21ac2ec83aa1b8ec7ada06bb8717ef9f
Achu-Exp <achu73743@gmail.com>
2025-01-24 15:03:32 +0530

    loading added for sbmit button in approval through email

--- src/app/Pages/rejected-response/rejected-response.component.ts
+++ src/app/Pages/rejected-response/rejected-response.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { EmailService } from '../../Services/email.service';
+import { NotificationService } from '../../Services/notification.service';
 
 @Component({
   selector: 'app-rejected-response',
@@ -22,7 +23,8 @@ export class RejectedResponseComponent {
     private api: ApiService,
     private route: ActivatedRoute,
     private fb: FormBuilder,
-    private email: EmailService
+    private email: EmailService,
+    private notification: NotificationService
   ) {
     this.rejectionForm = this.fb.group({
       reason: ['', Validators.required],
@@ -41,12 +43,15 @@ export class RejectedResponseComponent {
   get reasonControl() {
     return this.rejectionForm.get('reason');
   }
+  isSubmitting = false;
 
   submitReason(): void {
     if (this.rejectionForm.invalid) {
       this.rejectionForm.markAllAsTouched();
       return;
     }
+    this.isSubmitting = true;
+
 
     this.rejectionReason = this.rejectionForm.value.reason;
 
@@ -69,83 +74,91 @@ export class RejectedResponseComponent {
       const rejectionUpdates = {
         approvalStatus: 'Rejected',
         comments : this.rejectionReason
-      }  
-      this.api.updateReviewStatusAndComments(this.riskId, rejectionUpdates)
-    console.log('Risk ID:', this.riskId);
-    console.log('Approval Status', this.approvalStatus);
-    this.api.getRiskById(this.riskId).subscribe((res: any) => {
-      this.riskData = res;
-      if (res.riskStatus === 'open' || res.riskStatus === 'close') {
-        this.context = {
-          responsibleUser: res.createdBy.fullName,
-          riskId: res.riskId,
-          riskName: res.riskName,
-          description: res.description,
-          riskType: res.riskType,
-          impact: res.impact,
-          mitigation: res.mitigation,
-          plannedActionDate: new Date(res.plannedActionDate).toLocaleDateString(
-            'en-US',
-            {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            }
-          ),
-          overallRiskRating: res.overallRiskRating,
-          reason: this.rejectionReason,
-        };
-        // Send email to reviewer
-        this.email.sendOwnerEmail(res.createdBy.email, this.context).subscribe({
-          next: () => {
-            console.log('Reviewer Email:', res.createdBy.email);
-            console.log('Email Sent Successfully.');
-          },
-          error: (emailError) => {
-            console.error('Failed to send email to reviewer:', emailError);
-          },
-        });
       }
-      if(res.riskStatus === 'close'){
-
-        this.context = {
-          responsibleUser: res.responsibleUser.fullName,
-          riskId: res.riskId,
-          riskName: res.riskName,
-          description: res.description,
-          riskType: res.riskType,
-          impact: res.impact,
-          mitigation: res.mitigation,
-          plannedActionDate: new Date(res.plannedActionDate).toLocaleDateString(
-            'en-US',
-            {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
+      this.api.updateReviewStatusAndComments(this.riskId, rejectionUpdates).subscribe({
+        next: () => {
+          console.log('Risk ID:', this.riskId);
+          console.log('Approval Status', this.approvalStatus);
+          this.api.getRiskById(this.riskId).subscribe((res: any) => {
+            this.riskData = res;
+            if (res.riskStatus === 'open' || res.riskStatus === 'close') {
+              this.context = {
+                responsibleUser: res.createdBy.fullName,
+                riskId: res.riskId,
+                riskName: res.riskName,
+                description: res.description,
+                riskType: res.riskType,
+                impact: res.impact,
+                mitigation: res.mitigation,
+                plannedActionDate: new Date(res.plannedActionDate).toLocaleDateString(
+                  'en-US',
+                  {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  }
+                ),
+                overallRiskRating: res.overallRiskRating,
+                reason: this.rejectionReason,
+              };
+              // Send email to reviewer
+              this.email.sendOwnerEmail(res.createdBy.email, this.context).subscribe({
+                next: () => {
+                  console.log('Reviewer Email:', res.createdBy.email);
+                  console.log('Email Sent Successfully.');
+                },
+                error: (emailError) => {
+                  console.error('Failed to send email to reviewer:', emailError);
+                },
+              });
             }
-          ),
-          overallRiskRating: res.overallRiskRating,
-          reason: this.rejectionReason,
-        };
-        // Send email to reviewer
-        this.email.sendOwnerEmail(res.responsibleUser.email, this.context).subscribe({
-          next: () => {
-            console.log('Reviewer Email:', res.responsibleUser.email);
-            console.log('Email Sent Successfully.');
-          },
-          error: (emailError) => {
-            console.error('Failed to send email to reviewer:', emailError);
-          },
-        });
+            if(res.riskStatus === 'close'){
 
-      }
-    });
+              this.context = {
+                responsibleUser: res.responsibleUser.fullName,
+                riskId: res.riskId,
+                riskName: res.riskName,
+                description: res.description,
+                riskType: res.riskType,
+                impact: res.impact,
+                mitigation: res.mitigation,
+                plannedActionDate: new Date(res.plannedActionDate).toLocaleDateString(
+                  'en-US',
+                  {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  }
+                ),
+                overallRiskRating: res.overallRiskRating,
+                reason: this.rejectionReason,
+              };
+              // Send email to reviewer
+              this.email.sendOwnerEmail(res.responsibleUser.email, this.context).subscribe({
+                next: () => {
+                  console.log('Reviewer Email:', res.responsibleUser.email);
+                  console.log('Email Sent Successfully.');
+                },
+                error: (emailError) => {
+                  console.error('Failed to send email to reviewer:', emailError);
+                },
+              });
 
+            }
+          });
 
-    
+        },error: (error) => {
+          console.error('Error updating review status:', error);
+          this.notification.error('Failed to Reject risk');
+        },
+        complete: () => {
+          this.isSubmitting = false;
+        }
+      });
+
   }
 
   ngOnInit() {
-    
+
   }
 }
