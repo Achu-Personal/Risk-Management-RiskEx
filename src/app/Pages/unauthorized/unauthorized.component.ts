36dface7cda7f24182b816bc449e185d6045ad88
Achu-Exp <achu73743@gmail.com>
2025-04-17 15:40:26 +0530

    Unauthorized page modified into deactivate and permission

--- src/app/Pages/unauthorized/unauthorized.component.ts
+++ src/app/Pages/unauthorized/unauthorized.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './unauthorized.component.html',
   styleUrl: './unauthorized.component.scss'
 })
-export class UnauthorizedComponent {
-  constructor(private router: Router) {}
+export class UnauthorizedComponent implements OnInit {
+  errorType: string = 'permission'; 
 
-  navigateToLogin(): void {
-    this.router.navigate(['/sso']);
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Get error type from query parameters
+    this.route.queryParams.subscribe(params => {
+      if (params['type']) {
+        this.errorType = params['type'];
+      }
+    });
   }
 }
--- src/app/core/interceptors/token-interceptor.interceptor.ts
+++ src/app/core/interceptors/token-interceptor.interceptor.ts
@@ -4,28 +4,21 @@ import { AuthService } from '../../Services/auth/auth.service';
 
 export const tokenInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
 
-
-    const token = inject(AuthService).getToken()
+  const token = inject(AuthService).getToken()
 
     if (shouldAddToken(req)) {
       const newReq = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${token}`),
       });
       return next(newReq);
-
     }
-
     return next(req);
-
-
 };
-
-
 
 function shouldAddToken(req: HttpRequest<any>): boolean {
   const excludedUrls = [
     '/api/AuthControllers/Login',
-    'api/AuthControllers/SSOlogin',
+    '/api/AuthControllers/ssologin',
     'api/ResetPass/save-reset-token',
     'api/ResetPass/reset-password',
     'api/emails'
