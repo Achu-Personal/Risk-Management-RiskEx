4d3fc11c718aee3beb0d1e4253b92101894db2cd
Achu-Exp <achu73743@gmail.com>
2025-04-30 12:02:54 +0530

    risktypename filtering isuue rsolved

--- src/app/Pages/draft-page/draft-page.component.ts
+++ src/app/Pages/draft-page/draft-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { BodyContainerComponent } from "../../Components/body-container/body-container.component";
-import { ReusableTableComponent } from "../../Components/reusable-table/reusable-table.component";
+import { BodyContainerComponent } from '../../Components/body-container/body-container.component';
+import { ReusableTableComponent } from '../../Components/reusable-table/reusable-table.component';
 import { Router } from '@angular/router';
 import { ApiService } from '../../Services/api.service';
 import { AuthService } from '../../Services/auth/auth.service';
@@ -9,164 +9,155 @@ import { FormConformPopupComponent } from '../../Components/form-conform-popup/f
 @Component({
   selector: 'app-draft-page',
   standalone: true,
-  imports: [BodyContainerComponent, ReusableTableComponent,FormConformPopupComponent],
+  imports: [
+    BodyContainerComponent,
+    ReusableTableComponent,
+    FormConformPopupComponent,
+  ],
   templateUrl: './draft-page.component.html',
-  styleUrl: './draft-page.component.scss'
+  styleUrl: './draft-page.component.scss',
 })
 export class DraftPageComponent {
- isLoading = false;
- isconformDelete:boolean=false
- conformDelteDraft:boolean=false
- rowData:string=''
+  isLoading = false;
+  isconformDelete: boolean = false;
+  conformDelteDraft: boolean = false;
+  rowData: string = '';
 
-  constructor(private router: Router,private api:ApiService,private auth:AuthService) {}
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private auth: AuthService
+  ) {}
 
+  headerData: any = [
+    'riskName',
+    'description',
+    'riskTypeName',
+    'overallRiskRatingBefore',
+    'departmentName',
+    'responsibleUser',
+    'plannedActionDate',
+  ];
 
+  headerDisplayMap: { [key: string]: string } = {
+    riskName: 'Risk Name',
+    description: 'Description',
+    riskTypeName: 'Risk Type',
+    overallRiskRatingBefore: 'CRR',
+    departmentName: 'Department ',
+    responsibleUser: 'Responsible User',
 
+    plannedActionDate: 'End Date',
+  };
 
-headerData:any=[
-"riskName","description","riskType","overallRiskRating",  "departmentName","responsibleUser","plannedActionDate",
-];
-
-
-headerDisplayMap: { [key: string]: string } = {
-
-
-  riskName: "Risk Name",
-  description: "Description",
-  riskType: "Risk Type",
-  overallRiskRating: "CRR",
-  departmentName:'Department ',
-  responsibleUser:'Responsible User',
-
-  plannedActionDate:"End Date",
-
-};
-
-tableBody:any[]=[
-  {
-
-    riskName: '',
-    description: '',
-    riskType: '',
-    overallRiskRating: 0,
-    departmentName:"",
-    responsibleUser:"",
-
-    plannedActionDate: Date,
-
-  },
-]
-
-ngOnInit()
-{
-    this.isLoading = true;
-    if(this.auth.getUserRole()=="Admin")
+  tableBody: any[] = [
     {
-      this.headerData=[
-        "riskName","description","riskType","overallRiskRating",  "departmentName","responsibleUser","plannedActionDate","Action"
+      riskName: '',
+      description: '',
+      riskTypeName: '',
+      overallRiskRatingBefore: 0,
+      departmentName: '',
+      responsibleUser: '',
+
+      plannedActionDate: Date,
+    },
+  ];
+
+  ngOnInit() {
+    this.isLoading = true;
+    if (this.auth.getUserRole() == 'Admin') {
+      this.headerData = [
+        'riskName',
+        'description',
+        'riskTypeName',
+        'overallRiskRatingBefore',
+        'departmentName',
+        'responsibleUser',
+        'plannedActionDate',
+        'Action',
       ];
 
-      this.tableBody=[
+      this.tableBody = [
         {
-
           riskName: '',
           description: '',
           riskTypeName: '',
           overallRiskRatingBefore: 0,
-          departmentName:"",
-          responsibleUserName:"",
+          departmentName: '',
+          responsibleUserName: '',
           plannedActionDate: Date,
-
         },
-      ]
+      ];
 
-
-
-
-      this.api.getDraftOfAdmin().subscribe((e:any)=>{
-
-        this.tableBody=e;
-        console.log("draftdata",e);
+      this.api.getDraftOfAdmin().subscribe((e: any) => {
+        this.tableBody = e;
+        console.log('draftdata', e);
 
         this.isLoading = false;
-      })
-    }
-    else{
-
-      this.headerData=[
-        "riskName","description","riskType","overallRiskRating",  "departmentName","responsibleUser","plannedActionDate","Action"
+      });
+    } else {
+      this.headerData = [
+        'riskName',
+        'description',
+        'riskTypeName',
+        'overallRiskRatingBefore',
+        'departmentName',
+        'responsibleUser',
+        'plannedActionDate',
+        'Action',
       ];
-      this.tableBody=[
+      this.tableBody = [
         {
-
           riskName: '',
           description: '',
           riskTypeName: '',
           overallRiskRatingBefore: 0,
-          departmentName:"",
-          responsibleUserName:"",
+          departmentName: '',
+          responsibleUserName: '',
           plannedActionDate: Date,
-
         },
-      ]
+      ];
 
-
-
-      this.api.getDraft(this.auth.getDepartmentId()).subscribe((e:any)=>{
-          console.log("draftdata",e);
-        this.tableBody=e;
+      this.api.getDraft(this.auth.getDepartmentId()).subscribe((e: any) => {
+        console.log('draftdata', e);
+        this.tableBody = e;
         this.isLoading = false;
-      })
-
+      });
     }
-
-
-
-
-}
-
-
-onDraftEdited(row: any) {
-  console.log("draftrowdata",row);
-    this.router.navigate([`/addrisk`],{queryParams:row});// depart name or id
-
-
-}
-
-conformDraftDelete(){
-  this.conformDelteDraft=true;
-  this.isconformDelete=false;
-  this.onDraftDeleted(this.rowData);
-
-}
-
-closeDeleteDraft(){
-  this.isconformDelete=false;
-
-}
-
-
-onDraftDeleted(row: any) {
-  this.rowData=row;
-
-  this.isconformDelete=true;
-  if(this.conformDelteDraft){
-    console.log("Draftid",row.id);
-  this.api.deleteDraft(row.id).subscribe((e:any)=>{
-  });
-
-  this.tableBody.forEach((item, index) => {
-    if (item.id === row.id) {
-      let templist=[...this.tableBody]
-      templist.splice(index, 1);
-      this.tableBody=[...templist]
-    }
-  })
-  this.isconformDelete=false;
-  this.conformDelteDraft=false;
   }
 
+  onDraftEdited(row: any) {
+    console.log('draftrowdata', row);
+    this.router.navigate([`/addrisk`], { queryParams: row }); 
+  }
 
-}
+  conformDraftDelete() {
+    this.conformDelteDraft = true;
+    this.isconformDelete = false;
+    this.onDraftDeleted(this.rowData);
+  }
+
+  closeDeleteDraft() {
+    this.isconformDelete = false;
+  }
+
+  onDraftDeleted(row: any) {
+    this.rowData = row;
+
+    this.isconformDelete = true;
+    if (this.conformDelteDraft) {
+      console.log('Draftid', row.id);
+      this.api.deleteDraft(row.id).subscribe((e: any) => {});
+
+      this.tableBody.forEach((item, index) => {
+        if (item.id === row.id) {
+          let templist = [...this.tableBody];
+          templist.splice(index, 1);
+          this.tableBody = [...templist];
+        }
+      });
+      this.isconformDelete = false;
+      this.conformDelteDraft = false;
+    }
+  }
 }
