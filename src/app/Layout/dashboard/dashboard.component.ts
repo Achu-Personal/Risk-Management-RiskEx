f9b83df83cac0c85c5dc81ac286e076039f0c9e5
Achu-Exp <achu73743@gmail.com>
2025-05-09 15:17:31 +0530

    cleared unwanted messages

--- src/app/Layout/dashboard/dashboard.component.ts
+++ src/app/Layout/dashboard/dashboard.component.ts
@@ -1,10 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../Components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../UI/navbar/navbar.component';
 import { CrumbsComponent } from '../../UI/crumbs/crumbs.component';
-import { ChangepasswordComponent } from "../../Components/changepassword/changepassword.component";
-import { UsericonDropdownComponent } from "../../UI/usericon-dropdown/usericon-dropdown.component";
+
 
 
 @Component({
@@ -14,9 +13,7 @@ import { UsericonDropdownComponent } from "../../UI/usericon-dropdown/usericon-d
     RouterOutlet,
     SidebarComponent,
     NavbarComponent,
-    CrumbsComponent,
-    ChangepasswordComponent,
-
+    CrumbsComponent
 ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -25,11 +22,11 @@ export class DashboardComponent {
   navbarData: any = {};
   isShowPopUp:boolean=false
   showChangePassword(): void {
-    this.isShowPopUp = true; // Show the change password component
+    this.isShowPopUp = true;
   }
 
   hideChangePassword(): void {
-    this.isShowPopUp = false; // Hide the change password component
+    this.isShowPopUp = false; 
   }
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
