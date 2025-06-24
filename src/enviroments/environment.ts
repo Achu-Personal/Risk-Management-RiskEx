8d7c676878651f39fcdd2eff8f64485f9a84c202
Achu-Exp <achu73743@gmail.com>
2025-06-24 17:28:01 +0530

    url changes

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   ssoConfig: {
     clientId: '4fdd2973-09a8-4c28-a786-16f63cf33207',
     authority: 'https://login.microsoftonline.com/5b751804-232f-410d-bb2f-714e3bb466eb',
-    redirectUri: 'http://localhost:4200/auth',
-    postLogoutRedirectUri: 'http://localhost:4200/sso'
+    redirectUri: 'https://riskex.experionglobal.dev/auth',
+    postLogoutRedirectUri: 'https://riskex.experionglobal.dev/'
   }
 };
