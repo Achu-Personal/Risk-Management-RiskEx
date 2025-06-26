e6f96ea55f97ba8293bece2c0cc0d3a4d612c001
Achu-Exp <achu73743@gmail.com>
2025-06-26 10:44:45 +0530

    URL changed

--- src/enviroments/environment.prod.ts
+++ src/enviroments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://riskex.experionglobal.dev/api',
-  frontendUrl: 'http://riskex.experionglobal.dev',
+  frontendUrl: 'https://riskex.experionglobal.dev',
   ssoConfig: {
     clientId: '4fdd2973-09a8-4c28-a786-16f63cf33207',
     authority: 'https://login.microsoftonline.com/5b751804-232f-410d-bb2f-714e3bb466eb',
